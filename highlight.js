f2aefdfcc9bf57a0cb6b6693c5303b9326fa4173
jonas-mio-ab <jonas.kalashnikov@gmail.com>
2014-02-01 22:54:54 +0100

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -5,28 +5,31 @@
  * @param {string} needle The value being searched for, otherwise known as the needle.
  * @return {string} This function returns a string with highlighted text.
  */
-function highlight( haystack, needle )
+function highlight( haystack, needle, wrapTags )
 {
-	var openTag = '<span class="hl">';
-	var closeTag = '</span>';
-	var highlighted = '';
+	var hlString = '';
 	var i = -1;
+    
+    if (typeof wrapTags[1] !== 'string') wrapTags = [ '<span style="background:#fff700;">', '</span>' ];
 
 	while ( haystack.length > 0 )
 	{
-		i = haystack.toLowerCase().indexOf( needle.toLowerCase(), i+1 );
+		i = haystack.toLowerCase().indexOf( needle.toLowerCase(), i + 1 );
 		if ( i < 0 )
 		{
-			highlighted += haystack;
+			hlString += haystack;
 			haystack = '';
 		}
 		else
 		{
-			highlighted += haystack.substring( 0, i ) + openTag + haystack.substr( i, needle.length ) + closeTag;
-			haystack = haystack.substr( i+needle.length );
+			hlString += haystack.substring( 0, i ) + 
+                        wrapTags[0] + 
+                        haystack.substr( i, needle.length ) + 
+                        wrapTags[1];
+			haystack  = haystack.substr( i + needle.length );
 			i = -1;
 		}
 	}
 	
-	return highlighted;
+	return hlString;
 }
